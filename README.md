4f6c59386577918088dd87fffcd0be3358874719
Chris Mędrela <chris.medrela+github@gmail.com>
2018-05-18 22:47:36 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # 2018-06-06-prework
-Prework do szkolenia ze wzorców projektowych w Pythonie
+
+Prework do szkoleń ze wzorców projektowych w 2018.06.
+
+# Setup
+
+Do wykonania preworku, jak i podczas szkolenia, potrzebujemy mieć skonfigurowane laptopy.
+W tym celu wykonujemy następujące czynności:
+
+1. Instalujemy Pythona 3, najlepiej najnowszego (3.6.5). 
+   Pythona można pobrać i zainstalować [z oficjalnej strony](https://www.python.org/downloads/). 
+   Nie potrzebujemy instalować żadnych bibliotek.
+1. Potrzebujemy git'a. W tym celu:
+    - Pod Windowsem [instalujemy gitbash'a](https://gitforwindows.org).
+    - Pod Unixami powinien już być zainstalowany.
+    - Pod Mac'iem ściągamy [instalujemy git'a](https://git-scm.com/download/mac)
+1. Sprawdzamy w terminalu/gitbashu poprawność konfiguracji:
+
+   ```bash
+   $ python --version  # ewentualnie python3 --version
+   Python 3.6.5
+   $ git --version
+   git version 2.17.0
+   ```
+
+# Prework
+
+Prework składa się z dwóch zadań, z których tylko pierwsze (`exercise-1.py`) jest obowiązkowe.
+Drugie (`exercise-2.py`) jest dla chętnych.
+Rozwiązanie preworku wysyłamy jako dwa Pull Requesty na GitHubie.
+Poniżej przedstawiam najprostszy sposób, nie wymagający klonowania repo.
+
+1. Zakładamy konto na [GitHubie](https://github.com). Konieczna jest weryfikacja konta poprzez kliknięcie na link, który otrzymamy mailem.
+2. Wchodzimy na [stronę z repozytorium z preworkiem](https://github.com/chrismedrela/2018-06-06-prework)
+3. Otwieramy plik `exercise-1.py`.
+4. Rozwiązujemy zadanie (najlepiej jest skopiować kod na swoją maszynę)
+5. Następnie klikamy ikonę ołówka po prawej, tuż nad zawartością pliku.
+6. Wklejamy rozwiązanie i klikamy "Propose file change".
+7. Klikamy "Create Pull Request".
+8. Klikamy "Create Pull Request" jeszcze raz.
+9. Jeżeli mamy jeszcze czas i ochotę, powtarzamy kroki 3-8 dla drugiego zadania znajdującego się w pliku `exercise-2.py`.
---
+++ exercise-2.py
@@ -0,0 +1,150 @@
+# encoding: utf-8
+
+"""
+Open Close Principle to jedna z pięciu zasad SOLID. Mówi ona, że klasy i funkcje
+powinny być otwarte na rozszerzenie i zamknięte na modyfikacje. Innymi słowy,
+powinno być możliwe ponowne wykorzystanie kodu bez modyfikowania go.
+
+Poniżej znajduje się funkcja `calculate_discount_percentage`, która jest pewną
+strategią wyliczania przysługującej klientowi zniżki wyrażonej w procentach.
+Każdy klient jest instancją klasy Customer i ma trzy pola:
+
+- first_purchase_date -- data pierwszego zakupu lub None, jeżeli jest to nowy
+  klient
+- birth_date -- data urodzenia
+- is_veteran -- flaga informująca, czy dany klient jest VIPem
+
+Zaimplementowano następującą strategię:
+
+- seniorzy (65+) dostają 5% zniżki
+- lojalni klienci, którzy są z nami od 1, 5 lub 10 lat, dostają odpowiednio 10%,
+  12% i 20% zniżki,
+- nowi klienci dostają 15% zniżki
+- veterani dostają 10% zniżki
+- pozostali dostają 0% zniżki
+- jeżeli przysługuje więcej niż jedna zniżka, zniżki nie sumują się i wybieramy
+  tę największą
+
+Dodatkowo, niżej widzimy testy jednostkowe tej funkcji.
+
+W tej funkcji złamano zasadę otwarte-zamknięte, ponieważ nie ma możliwości
+ponownego wykorzystania jej.
+
+Jako ćwiczenie, postaraj się zrefaktoryzować kod tak, aby możliwe było ponowne
+użycie fragmentów obecnej strategii. Innymi słowy, powinno być możliwe
+zaimplementowanie podobnej strategii (w której, przykładowo, nie ma zniżki dla
+nowych klientów), bez duplikowania kodu.
+"""
+
+import datetime
+import unittest
+
+
+class Customer:
+    def __init__(self, first_purchase_date, birth_date, is_veteran):
+        assert isinstance(first_purchase_date, (type(None), datetime.datetime))
+        assert isinstance(birth_date, datetime.datetime)
+        assert isinstance(is_veteran, bool)
+
+        self.first_purchase_date = first_purchase_date
+        self.birth_date = birth_date
+        self.is_veteran = is_veteran
+
+
+def calculate_discount_percentage(customer):
+    discount = 0
+    now = datetime.datetime.now()
+    year = datetime.timedelta(days=365)
+    if customer.birth_date <= now - 65*year:
+        # senior discount
+        discount = 5
+    if customer.first_purchase_date is not None:
+        if customer.first_purchase_date <= now - year:
+            # after one year, loyal customers get 10%
+            discount = 10
+            if customer.first_purchase_date <= now - 5*year:
+                # after five years, 12%
+                discount = 12
+                if customer.first_purchase_date <= now - 10*year:
+                    # after ten years, 20%
+                    discount = 20
+    else:
+        # first time purchase ==> 15% discount
+        discount = 15
+    if customer.is_veteran:
+        discount = max(discount, 10)
+    return discount
+
+
+class CalculateDiscountPercentageTests(unittest.TestCase):
+    def setUp(self):
+        self.now = datetime.datetime.now()
+        self.year = datetime.timedelta(days=365)
+
+    def test_should_return_zero_for_casual_customer(self):
+        customer = Customer(first_purchase_date=self.now,
+                            birth_date=self.now-20*self.year,
+                            is_veteran=False)
+        got = calculate_discount_percentage(customer)
+        expected = 0
+        self.assertEqual(got, expected)
+
+    def test_should_return_15_for_new_client(self):
+        customer = Customer(first_purchase_date=None,
+                            birth_date=self.now-20*self.year,
+                            is_veteran=False)
+        got = calculate_discount_percentage(customer)
+        expected = 15
+        self.assertEqual(got, expected)
+
+    def test_should_return_10_for_veteran(self):
+        customer = Customer(first_purchase_date=self.now,
+                            birth_date=self.now-20*self.year,
+                            is_veteran=True)
+        got = calculate_discount_percentage(customer)
+        expected = 10
+        self.assertEqual(got, expected)
+
+    def test_should_return_5_for_a_senior(self):
+        customer = Customer(first_purchase_date=self.now,
+                            birth_date=self.now-65*self.year,
+                            is_veteran=False)
+        got = calculate_discount_percentage(customer)
+        expected = 5
+        self.assertEqual(got, expected)
+
+    def test_should_return_10_for_a_loyal_customer(self):
+        customer = Customer(first_purchase_date=self.now-1*self.year,
+                            birth_date=self.now-20*self.year,
+                            is_veteran=False)
+        got = calculate_discount_percentage(customer)
+        expected = 10
+        self.assertEqual(got, expected)
+
+    def test_should_return_12_for_a_more_loyal_customer(self):
+        customer = Customer(first_purchase_date=self.now-5*self.year,
+                            birth_date=self.now-20*self.year,
+                            is_veteran=False)
+        got = calculate_discount_percentage(customer)
+        expected = 12
+        self.assertEqual(got, expected)
+
+    def test_should_return_20_for_a_most_loyal_customer(self):
+        customer = Customer(first_purchase_date=self.now-10*self.year,
+                            birth_date=self.now-20*self.year,
+                            is_veteran=False)
+        got = calculate_discount_percentage(customer)
+        expected = 20
+        self.assertEqual(got, expected)
+
+    def test_should_return_maximum_discount(self):
+        customer = Customer(first_purchase_date=None,
+                            birth_date=self.now-20*self.year,
+                            is_veteran=True)
+        # eligible for 15% discount as a new client and 10% as a veteran
+        got = calculate_discount_percentage(customer)
+        expected = 15
+        self.assertEqual(got, expected)
+
+if __name__ == "__main__":
+    unittest.main()
