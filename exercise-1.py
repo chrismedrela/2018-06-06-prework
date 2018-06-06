e4fc5dceaf5360af4580369fc25c1470b774d5d1
Łukasz Matysiak <december0123@users.noreply.github.com>
2018-06-06 22:10:51 +0200

    Update exercise-1.py
    Łukasz Matysiak

--- exercise-1.py
+++ exercise-1.py
@@ -6,7 +6,6 @@ powinna mieć tylko jeden obszar odpowiedzialności i tylko jeden powód do zmia
 Poniżej przedstawiono klasę Person, która reprezentuje pojedynczą osobę - jej
 imię, nazwisko oraz adres mailowy. Jest to przykład złamania tej zasady,
 ponieważ w środku niej znajduje się walidacja adresu email.
-
 Jako ćwiczenie, spróbuj zrefaktoryzować ten kod tak, aby sama walidacja znalazła
 się w osobnej klasie reprezentującej adres email.
 """
@@ -15,8 +14,13 @@ import re
 import unittest
 
 
-EMAIL_PATTERN = re.compile(r'^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$')
-
+class Email(str):
+    email_pattern = re.compile(r'^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$')
+    def __new__(cls, email):
+        if cls.email_pattern.match(email) is None:
+            raise ValueError('Invalid email')
+        return email
+    
 
 class Person:
     def __init__(self, first_name, last_name, email):
@@ -26,7 +30,20 @@ class Person:
         
         self.first_name = first_name 
         self.last_name = last_name
-        if EMAIL_PATTERN.match(email) is None:
-            raise ValueError('Invalid email')
-        else:
-            self.email = email
+        self.email = Email(email)
+
+class PersonTests(unittest.TestCase):
+    def test_should_pass(self):
+        p = Person("Name", "Name", "name@name.com")
+        
+    def test_should_raise_on_space_instead_of_at(self):
+        with self.assertRaises(ValueError):
+            Person("name", "name", "name name.com")
+    
+    def test_should_raise_on_missing_domain(self):
+        with self.assertRaises(ValueError):
+            Person("name", "name", "name@name")
+    
+    def test_should_raise_on_missing_email(self):
+        with self.assertRaises(ValueError):
+            Person("name", "name", "")
