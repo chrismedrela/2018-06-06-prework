084e0b6a21a00861d3d3387225e1b376dc56c029
acichon <mamadreqju@gmail.com>
2018-06-11 10:22:46 +0200

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -6,14 +6,12 @@ powinna mieć tylko jeden obszar odpowiedzialności i tylko jeden powód do zmia
 Poniżej przedstawiono klasę Person, która reprezentuje pojedynczą osobę - jej
 imię, nazwisko oraz adres mailowy. Jest to przykład złamania tej zasady,
 ponieważ w środku niej znajduje się walidacja adresu email.
-
 Jako ćwiczenie, spróbuj zrefaktoryzować ten kod tak, aby sama walidacja znalazła
 się w osobnej klasie reprezentującej adres email.
 """
 
 import re
 import unittest
-
 
 EMAIL_PATTERN = re.compile(r'^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$')
 
@@ -22,11 +20,19 @@ class Person:
     def __init__(self, first_name, last_name, email):
         assert isinstance(first_name, str)
         assert isinstance(last_name, str)
-        assert isinstance(email, str)
-        
-        self.first_name = first_name 
+
+        self.first_name = first_name
         self.last_name = last_name
-        if EMAIL_PATTERN.match(email) is None:
+        self.email = EmailAddress(email).validate()
+
+
+class EmailAddress(object):
+    def __init__(self, email):
+        self.email = email
+
+    def validate(self):
+        assert isinstance(self.email, str)
+        if EMAIL_PATTERN.match(self.email) is None:
             raise ValueError('Invalid email')
         else:
-            self.email = email
+            return self.email
