0858d4206a8f01c7ee1768ed861b9c86fcc0e4a3
Kleju0011 <michal.strumecki@wp.pl>
2018-06-18 11:53:22 +0200

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,21 +1,20 @@
 # encoding: utf-8
 
-"""
-Single Responsibility Principle to jedna z zasad SOLID. Mówi ona, że każda klasa
-powinna mieć tylko jeden obszar odpowiedzialności i tylko jeden powód do zmiany.
-Poniżej przedstawiono klasę Person, która reprezentuje pojedynczą osobę - jej
-imię, nazwisko oraz adres mailowy. Jest to przykład złamania tej zasady,
-ponieważ w środku niej znajduje się walidacja adresu email.
-
-Jako ćwiczenie, spróbuj zrefaktoryzować ten kod tak, aby sama walidacja znalazła
-się w osobnej klasie reprezentującej adres email.
-"""
-
 import re
 import unittest
 
-
 EMAIL_PATTERN = re.compile(r'^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$')
+
+
+class EmailValidation:
+    def __init__(self, email_to_validate):
+        if EMAIL_PATTERN.match(email_to_validate) is None:
+            raise ValueError('Invalid email')
+        else:
+            self.email = email_to_validate
+
+    def __str__(self):
+        return self.email
 
 
 class Person:
@@ -23,10 +22,7 @@ class Person:
         assert isinstance(first_name, str)
         assert isinstance(last_name, str)
         assert isinstance(email, str)
-        
-        self.first_name = first_name 
+
+        self.first_name = first_name
         self.last_name = last_name
-        if EMAIL_PATTERN.match(email) is None:
-            raise ValueError('Invalid email')
-        else:
-            self.email = email
+        self.email = EmailValidation(email)
