b691927b15d2748a95954d4044050ac4f337ffe0
Agnieszka Weiske <aweiske1@gmail.com>
2018-06-19 12:08:53 +0200

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -6,13 +6,13 @@ powinna mieć tylko jeden obszar odpowiedzialności i tylko jeden powód do zmia
 Poniżej przedstawiono klasę Person, która reprezentuje pojedynczą osobę - jej
 imię, nazwisko oraz adres mailowy. Jest to przykład złamania tej zasady,
 ponieważ w środku niej znajduje się walidacja adresu email.
-
 Jako ćwiczenie, spróbuj zrefaktoryzować ten kod tak, aby sama walidacja znalazła
 się w osobnej klasie reprezentującej adres email.
 """
 
 import re
 import unittest
+from unittest import TestCase
 
 
 EMAIL_PATTERN = re.compile(r'^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$')
@@ -22,11 +22,39 @@ class Person:
     def __init__(self, first_name, last_name, email):
         assert isinstance(first_name, str)
         assert isinstance(last_name, str)
-        assert isinstance(email, str)
-        
-        self.first_name = first_name 
+
+        self.first_name = first_name
         self.last_name = last_name
+        self.email = EmailAddress(email)
+
+
+
+class EmailAddress:
+    def __init__(self, email):
+        self.email = self.validate(email)
+
+    def validate(self, email):
+        assert isinstance(email, str)
+
         if EMAIL_PATTERN.match(email) is None:
             raise ValueError('Invalid email')
         else:
-            self.email = email
+            return email
+
+
+class EmailAddressTest(TestCase):
+    def test_can_store_valid_address(self):
+        valid_email = "jan.kowalski@gmail.com"
+
+        email = EmailAddress(valid_email)
+
+        self.assertIsNotNone(email)
+
+    def test_can_return_error_when_invalid_address(self):
+        invalid_email = "jan.kowalski"
+
+        self.assertRaises(ValueError, EmailAddress, invalid_email)
+
+
+if __name__ == "__main__":
+ unittest.main()
